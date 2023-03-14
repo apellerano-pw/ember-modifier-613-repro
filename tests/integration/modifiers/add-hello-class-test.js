041178e89e641df70d6cdd4f2c57bd845c975148
Andrew Pellerano <23562652+apellerano-pw@users.noreply.github.com>
2023-03-14 16:30:11 -0400

    Add positive test for comparison

--- tests/integration/modifiers/add-hello-class-test.js
+++ tests/integration/modifiers/add-hello-class-test.js
@@ -18,4 +18,17 @@ module('Integration | Modifier | add-hello-class', function (hooks) {
     await clearRender();
     assert.false(document.body.classList.contains('hello'));
   });
+
+  test('it calls the destructor', async function (assert) {
+    this.set('enabled', false);
+    await render(hbs`
+      <div {{(modifier 'add-hello-class')}}>
+        qwerty
+      </div>
+    `);
+    this.set('enabled', true);
+    assert.true(document.body.classList.contains('hello'));
+    await clearRender();
+    assert.false(document.body.classList.contains('hello'));
+  });
 });
